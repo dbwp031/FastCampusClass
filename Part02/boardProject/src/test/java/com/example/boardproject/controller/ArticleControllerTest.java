c1e5b9ad09d43d3e798baf3df49c692d1ca9bcea
Yuje Lee <dbwp031@gmail.com>
2023-08-09 16:56:04 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하게 열어줌
    뷰 적절히 뼈대 잡음
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 때문에 (charset=UTF-8) wjrwjfgl xptmxm aothem tnwjd

--- Part02/boardProject/src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
+++ Part02/boardProject/src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
@@ -19,7 +19,7 @@ class ArticleControllerTest {
     public ArticleControllerTest(@Autowired MockMvc mvc) { // test에서는 @Autowired 생략하면 안됨
         this.mvc = mvc;
     }
-    @Disabled("구현 중")
+//    @Disabled("구현 중")
     @DisplayName("[view] [GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -28,7 +28,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles")) // cntrl + space + space -> suggestions & alt + enter -> import statically
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)) // contentType -> exact하게 맞을때만 통과 & contentTypeCompatibleWith -> 호환 가능한 애들까지 통과
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles")); // -> model attribute의 Map에 articles라는 키가 있는지 확인
     }
