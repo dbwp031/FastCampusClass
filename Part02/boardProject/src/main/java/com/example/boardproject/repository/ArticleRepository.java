914e308019878db6fa92394229620d5fa40fd58e
Yuje Lee <dbwp031@gmail.com>
2023-08-10 21:08:48 +0900

    #18 - Repository 업데이트
    Test code 작성하며 추후 필요할 코드 추가

--- Part02/boardProject/src/main/java/com/example/boardproject/repository/ArticleRepository.java
+++ Part02/boardProject/src/main/java/com/example/boardproject/repository/ArticleRepository.java
@@ -4,6 +4,8 @@ import com.example.boardproject.domain.Article;
 import com.example.boardproject.domain.QArticle;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
@@ -16,6 +18,7 @@ public interface ArticleRepository extends
         QuerydslPredicateExecutor<Article>, // 기본적인 검색 기능
         QuerydslBinderCustomizer<QArticle> //추가적인 customizing 검색 기능 (BinderCustomizer는 QClass를 넣어야 함.)
 {
+    Page<Article> findByTitle(String title, Pageable pageable);
     @Override // 이 안의 내용으로 검색에 대한 세부적인 규칙이 변경된다.
     default void customize(QuerydslBindings bindings, QArticle root){
         bindings.excludeUnlistedProperties(true); // 현재 QuerydslPredicateExecutor로 인해서 검색에 대하여 모든 필드가 열려있다. -> 필드를 선택해서 해당 필드만 검색할 수 있도록 하고 싶다.
