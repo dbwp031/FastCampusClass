d6e68d5de7f272df7dcd671c58bad02d45ab655b
Yuje Lee <dbwp031@gmail.com>
2023-08-10 20:55:19 +0900

    #18 - DTO 추가
    도메인 코드와 DTO 코드를 확실히 나누어서 서비스 코드에서 마무리된 데이터를 컨트롤러로 옮기게 끔 함.

---
+++ Part02/boardProject/src/main/java/com/example/boardproject/dto/ArticleDto.java
@@ -0,0 +1,53 @@
+package com.example.boardproject.dto;
+
+import com.example.boardproject.domain.Article;
+
+import java.time.LocalDateTime;
+/*레코드(record)란 "데이터 클래스"이며 순수하게 데이터를 보유하기 위한 특수한 종류의 클래스이다.
+코틀린의 데이터 클래스와 비슷한 느낌이라고 보면 된다. 밑에서 코드를 보겠지만,
+record 클래스를 정의할때, 그 모양은 정말 데이터의 유형만 딱 나타내는 듯한 느낌이다.
+훨씬더 간결하고 가볍기 때문에 Entity 혹은 DTO 클래스를 생성할때 사용되면 굉장히 좋을 듯하다.*/
+public record ArticleDto(
+        Long id,
+        UserAccountDto userAccountDto,
+        String title,
+        String content,
+        String hashtag,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+){
+    /*
+    * Effective Java
+    * 아이템 1: 생성자 대신 정적 팩터리 메서드를 고려하라
+    * of: 여러 매개변수를 받아 적합한 타입의 인스턴스를 반환하는 집계 메서드
+    * */
+    public static ArticleDto of(Long id, UserAccountDto userAccountDto, String title, String content, String hashtag, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleDto(id, userAccountDto, title, content, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static ArticleDto from(Article entity) {
+        return new ArticleDto(
+                entity.getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getTitle(),
+                entity.getContent(),
+                entity.getHashtag(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public Article toEntity() {
+        return Article.of(
+                userAccountDto.toEntity(),
+                title,
+                content,
+                hashtag
+        );
+    }
+
+}
