741cdaa48570b7bd4077f5c5f37e69a0a190fd41
Yuje Lee <dbwp031@gmail.com>
2023-08-11 10:10:10 +0900

    #18- 비즈니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- Part02/boardProject/src/main/java/com/example/boardproject/service/ArticleService.java
+++ Part02/boardProject/src/main/java/com/example/boardproject/service/ArticleService.java
@@ -1,10 +1,14 @@
 package com.example.boardproject.service;
 
+import com.example.boardproject.domain.Article;
 import com.example.boardproject.domain.type.SearchType;
+import com.example.boardproject.dto.ArticleCommentDto;
 import com.example.boardproject.dto.ArticleDto;
 import com.example.boardproject.dto.ArticleWithCommentsDto;
 import com.example.boardproject.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 * 이 뜻은, 클래스 내부의 여러 매소드에 걸쳐 하나의 트랙잭션으로 사용한다는 의미로, 하나의 매소드에서 에러가 발생하면 다른 매소드에 영향을 미칠 수 있다.
 * 반대로, 매소드 레벨에서 @Transactional을 걸어주면 메소드끼리 영향을 받지 않는다.
 * */
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -28,7 +33,16 @@ public class ArticleService {
     * */
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
-        return Page.empty();
+        if (searchKeyword == null || searchKeyword.isBlank()) {
+            return articleRepository.findAll(pageable).map(ArticleDto::from);
+        }
+        return switch (searchType) {
+            case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case ID -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case NICKNAME ->articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case HASHTAG -> articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
+        };
     }
     @Transactional(readOnly = true)
     public ArticleDto searchArticle(long l) {
@@ -36,15 +50,32 @@ public class ArticleService {
     }
     @Transactional(readOnly = true)
     public ArticleWithCommentsDto getArticle(Long articleId) {
-        return null;
+        return articleRepository.findById(articleId)
+                .map(ArticleWithCommentsDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
     public void saveArticle(ArticleDto dto) {
+        articleRepository.save(dto.toEntity());
     }
 
     public void updateArticle(ArticleDto dto) {
+        try {
+            Article article = articleRepository.getReferenceById(dto.id());
+            if (dto.title() != null) {
+                article.setTitle(dto.title());
+            }
+            if (dto.content() != null) {
+                article.setContent(dto.content());
+            }
+            article.setHashtag(dto.hashtag());
+        } catch (EntityNotFoundException e) {
+            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다 - dto: {}");
+        }
     }
 
+
     public void deleteArticle(Long articleId) {
+        articleRepository.deleteById(articleId);
     }
 
 }
