08dbca70753a4a9ed391329418c76057fc81269c
Yuje Lee <dbwp031@gmail.com>
2023-08-11 21:52:49 +0900

    #18 - 게시판 서비스 뷰에 기능 구현하기 - 댓글 기능 구현
    구현하지 못했던 댓글 기능들을 구현하였다.

--- Part02/boardProject/src/main/java/com/example/boardproject/service/ArticleCommentsService.java
+++ Part02/boardProject/src/main/java/com/example/boardproject/service/ArticleCommentsService.java
@@ -1,13 +1,17 @@
 package com.example.boardproject.service;
 
+import com.example.boardproject.domain.ArticleComment;
 import com.example.boardproject.dto.ArticleCommentDto;
 import com.example.boardproject.repository.ArticleCommentRepository;
 import com.example.boardproject.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -23,8 +27,21 @@ public class ArticleCommentsService {
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
     public void updateArticleComment(ArticleCommentDto dto) {
+        try {
+            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
+            if (dto.content() != null) {
+                articleComment.setContent(dto.content());
+            }
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     public void deleteArticleComment(Long articleCommentId) {
