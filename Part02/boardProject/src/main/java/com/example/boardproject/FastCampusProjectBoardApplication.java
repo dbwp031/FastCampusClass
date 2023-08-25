03604584cd9ce25ebdbdfc39e8b8d70be6442507
Yuje Lee <dbwp031@gmail.com>
2023-08-25 17:46:39 +0900

    #50 - hashtag 변경에 따른 view 변경

--- Part02/boardProject/src/main/java/com/example/boardproject/FastCampusProjectBoardApplication.java
+++ Part02/boardProject/src/main/java/com/example/boardproject/FastCampusProjectBoardApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastCampusProjectBoardApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FastCampusProjectBoardApplication.class, args);
 	}
-
 }
